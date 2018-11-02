6288b5b7adc30fe3407719bd0adbecd8ddae0534
ohtorii <ikeuchi.h9@gmail.com>
2018-11-02 21:13:18 +0900

    Update ya_hidemaru_snippet/snippets/text-mode/c-mode/struct.%._.py

---
+++ ya_hidemaru_snippet/snippets/text-mode/c-mode/struct.%._.py
@@ -0,0 +1,47 @@
+"""
+# before
+struct vector float x float y float z
+
+# after
+struct vector{
+    float x;
+    float y;
+    float z;
+};
+
+"""
+import sys
+
+def Main():
+    head={"class":argv[0], "name":argv[1]}
+    
+    #
+    #struct StructName{
+    #
+    print("%(class)s %(name)s{" % head)
+    
+    #
+    #member
+    #
+    if len(argv)%2:
+        argv.append(" ")
+    n=len(argv)
+    s=""
+    index=0
+    for i in range(2,n,2):
+        typen_name = argv[i]
+        value_name = argv[i+1]
+        if index:
+            s=s+"\n";
+        tmp="\t%s %s;" % (typen_name,value_name)
+        s=s+tmp;
+        index=index+1;
+    print(s)
+
+    #
+    #finish
+    #
+    #print("};");
+    sys.stdout.write("};");
+
+Main()
