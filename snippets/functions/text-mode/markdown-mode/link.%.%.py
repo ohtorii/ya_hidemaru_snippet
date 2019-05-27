73485be909080ee85f0bdd5718943866ec955fbf
ohtorii <ikeuchi.h9@gmail.com>
2019-05-27 19:39:03 +0900

    Add MarkDown snippets.

---
+++ snippets/functions/text-mode/markdown-mode/link.%.%.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+"""
+# Before
+link https://www.google.com google
+link google https://www.google.com
+
+# After
+[google](https://www.google.com)|
+
+"""
+import sys
+
+def insert(title,link):
+    print("[%s](%s)%%|"%(title,link))
+
+def main(argv):
+    first=argv[1]
+    second=argv[2]
+    if "://" in first:
+        insert(second,first)
+    else:
+        insert(first,second)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ snippets/functions/text-mode/markdown-mode/link.%.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+"""
+# [1/2] Before
+link https://www.google.com
+
+# [1/2] After
+[|Title](https://www.google.com)
+
+# [2/2] Before
+link google
+
+# [1/2] After
+[google](https://|)
+
+"""
+import sys
+
+def main(argv):
+    first=argv[1]
+    if "://" in first:
+        print("[%%|Title](%s)"%first)
+    else:
+        print("[%s](https://%%|)"%first)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
